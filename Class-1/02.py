2f5f5a1761425b35439b4071b4bb451d9a42dc80
xd1603121705 <junlee_happy@163.com>
2020-03-15 00:23:32 +0800

    'update'

--- Class-1/02.py
+++ Class-1/02.py
@@ -1,1 +1,24 @@
-1.变量命名规则
+"""
+将华氏温度转变成摄氏温度
+
+Version: 1.0
+Author: junlee_happy@163.com
+"""
+import math
+
+f = float(input('请输入华氏温度：'))
+c = (f - 32) / 1.8
+print('%.1f华氏度 = %.1f摄氏度' % (f, c))
+
+"""
+输入半径计算圆的周长和面积
+
+Version: 1.0
+Author: jun
+"""
+
+radius = float(input('请输入圆的半径：'))
+perimeter = 2 * math.pi * radius
+area = math.pi * radius * radius
+print('周长：%.2f' % perimeter)
+print('面积：%.2f' % area)
