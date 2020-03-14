57e6cb9a1a31a212934caff57f8e17167fed83e1
xd1603121705 <junlee_happy@163.com>
2020-03-14 18:56:34 +0800

    add class 01-02

---
+++ Class-1/01.py
@@ -0,0 +1,18 @@
+# The Zen of Python, by Tim Peters
+import this
+
+# draw graphics
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('red')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()
